f247322c7b9bd3cb5e2af0a4c574dd9888d7ad59
Dmitry <dgrapov@gmail.com>
2018-07-03 20:41:57 -0500

    init

---
+++ NOTES.R
@@ -0,0 +1,24 @@
+#NOTES
+
+# update manual on github
+git checkout -b gh-pages
+
+#remove all and create index.html
+git checkout gh-pages
+cp inst/manual.html index.html
+find . -type f -not -name 'index.html' -not -name '.git' -delete # this deletes the git repo
+
+
+#build help pages for each package here
+#make UI for unified help pages
+#use bookdown for pages?
+
+#help pages
+-[ ] dave.main
+-[ ] dave.preproc
+-[ ] dave.stats
+-[ ] dave.cluster
+-[ ] dave.multivariate
+-[ ] dave.pathway
+-[ ] dave.rf
+-[ ] dave.network
