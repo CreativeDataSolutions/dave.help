c0dbee4cc084302eb9a5be1c09c1371a0f69072b
Dmitry <dgrapov@gmail.com>
2018-12-08 22:07:48 -0600

    init

--- R/formatting_helpers.R
+++
@@ -1,8 +0,0 @@
-
-#' @title signif_format_fun
-#' @export
-#' @details pretty format table values
-signif_format_fun<-function(x,digits=2){
-  as.numeric(x) %>%
-    formatC(., digits=digits,format="e", flag="#")
-}
